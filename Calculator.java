56a3d0bd963f107968d3a1d4f9dfb95286254df3
hryang06 <helengmlfls@gmail.com>
2017-08-09 14:33:23 +0900

    new minus method

--- Calculator.java
+++ Calculator.java
@@ -15,7 +15,13 @@ public class Calculator {
     return a + b;
   }
 
-  private static int minus(int a, int b) {
-    return a - b;
+  private static int minus(int a, int b, Object GPU) {
+    int result = 0;
+    if( GPU == null ) {
+      result = a - b;
+    } else {
+      result = a - b;
+    }
+    return result;
   }
 }
