5fd51c3d972533f1c3c828087cc37d0b050282fc
hryang06 <helengmlfls@gmail.com>
2017-08-09 11:19:51 +0900

    sum 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,12 @@ package com.mycompany.test;
 public class Calculator {
 
   public static void main(String[] args) {
+    int a = 4;
+    int b = 2;
+    System.out.println( sum(a,b) );
+  }
+
+  private static int sum(int a, int b) {
+    return a + b;
   }
 }
